0664a2a14298878f8057b6031e930d7515f7d8fc
SafetyMarcus <hooperarma@gmail.com>
2016-04-04 22:34:35 +1000

    Setting up first animation
    VSSSSSSSS!!!!

--- app/src/main/java/hoopray/safetypongandroid/ChallengeActivity.java
+++ app/src/main/java/hoopray/safetypongandroid/ChallengeActivity.java
@@ -1,17 +1,42 @@
 package hoopray.safetypongandroid;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * @author Marcus Hooper
  */
 public class ChallengeActivity extends AppCompatActivity
 {
+	@Bind(R.id.v)
+	View v;
+	@Bind(R.id.s)
+	View s;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.challenge_activity);
+		ButterKnife.bind(this);
+	}
+
+	@OnClick(R.id.p1_next) void onFirstNextClick()
+	{
+		AnimatorSet vsSet = new AnimatorSet();
+		ObjectAnimator vAnimator = ObjectAnimator.ofFloat(v, "translationX",
+				HelperFunctions.intDpToDisplayMetric(-48), HelperFunctions.intDpToDisplayMetric(-24));
+		ObjectAnimator sAnimator = ObjectAnimator.ofFloat(s, "translationX",
+				HelperFunctions.intDpToDisplayMetric(48), HelperFunctions.intDpToDisplayMetric(24));
+		ObjectAnimator vAlpha = ObjectAnimator.ofFloat(v, "alpha", 0, 1);
+		ObjectAnimator sAlpha = ObjectAnimator.ofFloat(s, "alpha", 0, 1);
+		vsSet.playTogether(vAnimator, sAnimator, vAlpha, sAlpha);
+		vsSet.start();
 	}
 }
--- app/src/main/java/hoopray/safetypongandroid/SafetyApplication.java
+++ app/src/main/java/hoopray/safetypongandroid/SafetyApplication.java
@@ -9,10 +9,17 @@ import android.os.Build;
 public class SafetyApplication extends Application
 {
 	public static boolean is21Plus = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+	private static SafetyApplication app;
 
 	@Override
 	public void onCreate()
 	{
 		super.onCreate();
+		app = this;
+	}
+
+	public static SafetyApplication getInstance()
+	{
+		return app;
 	}
 }
