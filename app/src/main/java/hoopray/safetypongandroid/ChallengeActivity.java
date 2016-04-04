a3859b39269c67cbf331ff5cd7c7c85a40c1f873
SafetyMarcus <hooperarma@gmail.com>
2016-04-05 00:21:03 +1000

    Adding in saving of name to view and more animations
    Added in forward and reverse states for view animations as well as animatin between TextView and EditText and Hiding/showing the next buttons

--- app/src/main/java/hoopray/safetypongandroid/ChallengeActivity.java
+++ app/src/main/java/hoopray/safetypongandroid/ChallengeActivity.java
@@ -7,9 +7,14 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Explode;
 import android.view.View;
+import android.view.animation.Interpolator;
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.easygoingapps.ThePolice;
+import com.easygoingapps.annotations.Observe;
+import com.easygoingapps.utils.State;
 
 /**
  * @author Marcus Hooper
@@ -24,6 +29,29 @@ public class ChallengeActivity extends AppCompatActivity
 	TextInputLayout secondChallengerLayout;
 	@Bind(R.id.p2_next)
 	View p2Next;
+	@Bind(R.id.p1_next)
+	View p1Next;
+	@Bind(R.id.p1_input)
+	TextInputLayout firstChallengerLayout;
+	@Bind(R.id.first_challenger_final)
+	TextView firstChallengerFinalView;
+	@Bind(R.id.second_challenger_final)
+	TextView secondChallengerFinalView;
+
+	AnimatorSet vsSet;
+	AnimatorSet hideSet;
+	AnimatorSet secondarySet;
+	AnimatorSet finalSet;
+
+	boolean hasAnimatedFirst;
+	boolean hasAnimatedSecond;
+
+	//Binding edit text to name
+	@Observe(R.id.first_challenger)
+	public State<String> firstChallenger = new State<>("");
+
+	@Observe(R.id.second_challenger)
+	public State<String> secondChallenger = new State<>("");
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -31,24 +59,37 @@ public class ChallengeActivity extends AppCompatActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.challenge_activity);
 		ButterKnife.bind(this);
+		ThePolice.watch(this);
 
 		if(SafetyApplication.is21Plus)
 			getWindow().setEnterTransition(new Explode());
 	}
 
-	@OnClick(R.id.p1_next) void onFirstNextClick()
+	@OnClick(R.id.p1_next)
+	void onFirstNextClick()
 	{
-		AnimatorSet vsSet = new AnimatorSet();
+		firstChallengerFinalView.setText(firstChallenger.getValue());
+
+		vsSet = new AnimatorSet();
 		ObjectAnimator vAnimator = ObjectAnimator.ofFloat(v, "translationX",
 				HelperFunctions.intDpToDisplayMetric(-48), HelperFunctions.intDpToDisplayMetric(-24));
 		ObjectAnimator sAnimator = ObjectAnimator.ofFloat(s, "translationX",
 				HelperFunctions.intDpToDisplayMetric(48), HelperFunctions.intDpToDisplayMetric(24));
 		ObjectAnimator vAlpha = ObjectAnimator.ofFloat(v, "alpha", 0, 1);
 		ObjectAnimator sAlpha = ObjectAnimator.ofFloat(s, "alpha", 0, 1);
+
 		vsSet.playTogether(vAnimator, sAnimator, vAlpha, sAlpha);
 		vsSet.start();
 
-		AnimatorSet secondarySet = new AnimatorSet();
+		hideSet = new AnimatorSet();
+		ObjectAnimator hideFirstAlpha = ObjectAnimator.ofFloat(firstChallengerLayout, "alpha", 1, 0);
+		ObjectAnimator showFirstFinalAlpha = ObjectAnimator.ofFloat(firstChallengerFinalView, "alpha", 0, 1);
+		ObjectAnimator hideFirstNext = ObjectAnimator.ofFloat(p1Next, "alpha", 1, 0);
+		hideSet.setStartDelay(100);
+		hideSet.playTogether(hideFirstAlpha, showFirstFinalAlpha, hideFirstNext);
+		hideSet.start();
+
+		secondarySet = new AnimatorSet();
 		ObjectAnimator secondPlayerAnimation = ObjectAnimator.ofFloat(secondChallengerLayout, "translationX",
 				HelperFunctions.intDpToDisplayMetric(-48), 0);
 		ObjectAnimator secondNextAnimation = ObjectAnimator.ofFloat(p2Next, "translationX",
@@ -59,5 +100,64 @@ public class ChallengeActivity extends AppCompatActivity
 		secondarySet.playTogether(secondPlayerAlpha, secondNextAlpha, secondPlayerAnimation, secondNextAnimation);
 		secondarySet.setStartDelay(100);
 		secondarySet.start();
+
+		hasAnimatedFirst = true;
+	}
+
+	@OnClick(R.id.p2_next)
+	void onFinishClick()
+	{
+		secondChallengerFinalView.setText(secondChallenger.getValue());
+
+		finalSet = new AnimatorSet();
+		ObjectAnimator hideFirstAlpha = ObjectAnimator.ofFloat(secondChallengerLayout, "alpha", 1, 0);
+		ObjectAnimator showFirstFinalAlpha = ObjectAnimator.ofFloat(secondChallengerFinalView, "alpha", 0, 1);
+		ObjectAnimator hideFirstNext = ObjectAnimator.ofFloat(p2Next, "alpha", 1, 0);
+		finalSet.setStartDelay(100);
+		finalSet.playTogether(hideFirstAlpha, showFirstFinalAlpha, hideFirstNext);
+		finalSet.start();
+
+		hasAnimatedSecond = true;
+	}
+
+	@Override
+	public void onBackPressed()
+	{
+		ReverseInterpolator interpolator = new ReverseInterpolator();
+
+		if(hasAnimatedSecond)
+		{
+			finalSet.setInterpolator(interpolator);
+			finalSet.start();
+			hasAnimatedSecond = false;
+			return;
+		}
+		if(hasAnimatedFirst)
+		{
+			vsSet.setInterpolator(interpolator);
+			vsSet.setStartDelay(100);
+			vsSet.start();
+
+			hideSet.setStartDelay(100);
+			hideSet.setInterpolator(interpolator);
+			hideSet.start();
+
+			secondarySet.setInterpolator(interpolator);
+			secondarySet.setStartDelay(0);
+			secondarySet.start();
+			hasAnimatedFirst = false;
+			return;
+		}
+
+		super.onBackPressed();
+	}
+
+	public class ReverseInterpolator implements Interpolator
+	{
+		@Override
+		public float getInterpolation(float paramFloat)
+		{
+			return Math.abs(paramFloat - 1f);
+		}
 	}
 }
